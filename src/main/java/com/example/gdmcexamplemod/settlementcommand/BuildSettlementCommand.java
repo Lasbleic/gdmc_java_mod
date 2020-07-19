7d6308d5e8903bd701a7b6e06967b906dc558701
Corentin LASBLEIS <lasbleis.corentin@gmail.com>
2020-07-19 13:03:22 +0200

    Creates a better abstraction with the 'buildSettlement' method

--- src/main/java/com/example/gdmcexamplemod/settlementcommand/BuildSettlementCommand.java
+++ src/main/java/com/example/gdmcexamplemod/settlementcommand/BuildSettlementCommand.java
@@ -20,19 +20,39 @@ public class BuildSettlementCommand<S> {
     private BuildSettlementCommand() { }
 
     public static void register(CommandDispatcher<CommandSource> dispatcher) {
-        dispatcher.register(Commands.literal(COMMAND_NAME)
-                .then(Commands.argument("from", BlockPosArgument.blockPos()).then(Commands.argument("to", BlockPosArgument.blockPos()).executes( context -> {
-                    return perform(context, BlockPosArgument.getLoadedBlockPos(context, "from"), BlockPosArgument.getLoadedBlockPos(context, "from"));
+        dispatcher.register(
+                Commands.literal(COMMAND_NAME)
+                .then(Commands.argument("from", BlockPosArgument.blockPos())
+                .then(Commands.argument("to", BlockPosArgument.blockPos())
+                .executes( context -> {
+                    return perform(context, BlockPosArgument.getLoadedBlockPos(context, "from"), BlockPosArgument.getLoadedBlockPos(context, "to"));
                 }))));
     }
 
     private static int perform(CommandContext<CommandSource> commandSourceContext, BlockPos from, BlockPos to) {
+
         ServerWorld world = commandSourceContext.getSource().getServer().getWorld(DimensionType.OVERWORLD);
+        CommandSource source = commandSourceContext.getSource();
+        int x1 = from.getX(), y1 = from.getY(), z1 = from.getZ();
+        int x2 = to.getX(), y2 = to.getY(), z2 = to.getZ();
 
-        LOGGER.info("BuildSettlement command has been performed");
+        int result = buildSettlement(source, world, x1, y1, z1, x2, y2, z2);
 
-        commandSourceContext.getSource().getEntity().sendMessage(new StringTextComponent("Hello World!"));
-        commandSourceContext.getSource().getEntity().sendMessage(new StringTextComponent("You want to build a settlement from " + from.toString() + " to " + to.toString()));
+        if (result > 0) {
+            LOGGER.info("BuildSettlement command has been performed");
+        } else {
+            LOGGER.error("BuildSettlement command has failed");
+        }
+
+        return result;
+    }
+
+    private static int buildSettlement(CommandSource source, ServerWorld world, int x1, int y1, int z1, int x2, int y2, int z2) {
+
+        source.getEntity().sendMessage(new StringTextComponent("Hello World!"));
+        source.getEntity().sendMessage(new StringTextComponent(String.format("You want to build a settlement from %d, %d, %d to %d, %d, %d,", x1, y1, z1, x2, y2, z2 )));
+
+        // TODO: generate a settlement
 
         return 1;
     }
